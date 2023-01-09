b8c7be348a33c1144e5a4dd3012c2c6182e377ef
Mattèo Gauthier <matteo.gauthier@gmail.com>
2023-01-09 11:10:52 +0100

    add filters

--- components/RoomViz.tsx
+++ components/RoomViz.tsx
@@ -1,45 +1,45 @@
-import React from "react"
-import clsx from "clsx"
-import {
-  Badge,
-  Button,
-  Card,
-  Chip,
-  Group,
-  Image,
-  Paper,
-  SegmentedControl,
-  SimpleGrid,
-  Switch,
-  Text,
-  Title,
-} from "@mantine/core"
+import { Badge, Button, Card, Group, Image, Paper, Select, SimpleGrid, Switch, Text, Title } from "@mantine/core"
 import { openContextModal } from "@mantine/modals"
-import { Room } from "../types"
-import fetcher from "../lib/fetcher"
+import { useCallback, useMemo, useState } from "react"
 import useSWR from "swr"
+import fetcher from "../lib/fetcher"
+import { colorFromString } from "../lib/utils"
+import { Building, Room } from "../types"
 
 type Props = {}
 
 export default function RoomViz({}: Props) {
+  const [filter, setFilter] = useState<"all" | string>("all")
+  const [showOnlyAvailable, setShowOnlyAvailable] = useState<boolean>(false)
   const { data: rooms } = useSWR<Room[]>("/api/room", fetcher)
+  const { data: buildings } = useSWR<Building[]>("/api/building", fetcher)
+
+  const buildingsFilter = useMemo(() => {
+    const list = buildings?.map((building) => ({ label: building.name, value: building.name })) || []
+    return [{ label: "Tout les bâtiments", value: "all" }, ...list]
+  }, [buildings])
+
+  const filteredRooms = useMemo(
+    () =>
+      rooms
+        ?.filter((r) => (showOnlyAvailable ? r.isAvailable : r))
+        .filter((r) => (filter == "all" ? r : r.building === filter)),
+    [filter, rooms, showOnlyAvailable]
+  )
+
+  const getColor = useCallback((e: string) => colorFromString(e), [])
 
   return (
     <Paper shadow="xs" p="md">
       <Title order={2}>Visualisation des salles de réunions</Title>
       <hr style={{ opacity: 0.2 }} />
       <Group mb={"md"} align={"center"} sx={{ justifyContent: "space-between" }}>
-        <Switch mt={8} label="Afficher uniquement les salles disponibles" />
-        <SegmentedControl
-          size={"sm"}
-          radius={"md"}
-          data={[
-            { label: "React", value: "react" },
-            { label: "Angular", value: "ng" },
-            { label: "Vue", value: "vue" },
-            { label: "Svelte", value: "svelte" },
-          ]}
+        <Switch
+          mt={8}
+          onChange={(e) => setShowOnlyAvailable(e.currentTarget.checked)}
+          label="Afficher uniquement les salles disponibles"
         />
+        <Select defaultValue={"all"} onChange={(v) => v && setFilter(v)} data={buildingsFilter} />
       </Group>
       <SimpleGrid
         cols={4}
@@ -50,17 +50,16 @@ export default function RoomViz({}: Props) {
           { maxWidth: 600, cols: 1, spacing: "sm" },
         ]}
       >
-        {rooms ? (
-          rooms.map((room) => (
+        {filteredRooms ? (
+          filteredRooms.map((room) => (
             <Card key={room.id} shadow="sm" p="sm" radius="md" withBorder>
               <Card.Section>
                 <Image src={"https://source.unsplash.com/weekly?" + room.id} height={110} alt="Norway" />
                 {room.building && (
                   <Badge
-                    sx={{ position: "absolute", top: 8, right: 6 }}
+                    sx={{ position: "absolute", top: 8, right: 6, backgroundColor: getColor(room.building) }}
                     radius={"md"}
                     variant="filled"
-                    color={"dark"}
                     size={"md"}
                   >
                     {room.building}
